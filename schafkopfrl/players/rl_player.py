7416d003f7f176a0c1ea8c10e1b17c1380044985
Tobias Emrich <tobias.emrich@harman.com>
2020-09-30 11:51:35 +0200

    re-included action shaping

--- schafkopfrl/players/rl_player.py
+++ schafkopfrl/players/rl_player.py
@@ -9,12 +9,16 @@ from schafkopfrl.utils import one_hot_games, one_hot_cards
 
 class RlPlayer(Player):
 
-  def __init__(self, policy):
+  def __init__(self, policy, action_shaping=True):
     super().__init__()
     self.memory = Memory()
     self.policy = policy
+    self.action_shaping = action_shaping
 
   def act(self, state):
+
+    if self.action_shaping and state["game_state"].game_stage == Rules.BIDDING:
+      state["allowed_actions"] = self.game_selection_shaping(state["allowed_actions"], state["allowed_actions"])
 
     encoded_state = self.policy.preprocess(state)
 
@@ -23,57 +27,48 @@ class RlPlayer(Player):
     dist = Categorical(action_probs)
     action = dist.sample()
 
+    action_prob = dist.probs[action].item()  # only for debugging purposes
+
     self.memory.states.append([s.detach() for s in encoded_state])
     self.memory.actions.append(action)
     self.memory.logprobs.append(dist.log_prob(action).detach())
 
-    action_prob = dist.probs[action].item()  # only for debugging purposes
-
     # translate output from NN to interpretable action
-    if action.item() < 9: # bidding round
+    if state["game_state"].game_stage == Rules.BIDDING:
       return self.rules.games[action], action_prob
-    elif action.item() < 11: # contra or retour
+    elif state["game_state"].game_stage == Rules.CONTRA or state["game_state"].game_stage == Rules.RETOUR:
       return action.item() - 9 == 0, action_prob
-    else: #play card
+    else: #trick stage
       return self.rules.cards[action - 11], action_prob
 
 
-  def call_game_type(self, game_state):
-
-    allowed_games = self.rules.allowed_games(self.cards)
-
+  def game_selection_shaping(self, player_cards, allowed_games):
 
     # hard coded solo and wenz decisions
     for solo in [[0, 2], [1, 2], [2, 2], [3, 2]]:
       trump_count = 0
       for card in self.rules.get_sorted_trumps(solo):
-        if card in self.cards:
+        if card in player_cards:
           trump_count+= 1
       if trump_count >= 7:
         allowed_games = [solo]
         break;
 
-    wenz_count = len([card for card in [[0, 3], [1, 3], [2, 3], [3, 3]] if card in self.cards])
+    wenz_count = len([card for card in [[0, 3], [1, 3], [2, 3], [3, 3]] if card in player_cards])
     spazen_count = 0
     if wenz_count >= 2:
       for color in range(4):
-        if [color, 7] in self.cards:
+        if [color, 7] in player_cards:
           continue
         for number in range(7):
           if number == 3:
             continue
-          if [color, number] in self.cards:
+          if [color, number] in player_cards:
             spazen_count += 1
       if spazen_count < 2:
         allowed_games = [[None, 1]]
 
-    action, prob = self.act(
-      self.policy.preprocess(game_state, self),
-      torch.tensor(np.concatenate((one_hot_games(allowed_games), np.zeros(2), np.zeros(32)))).float())
-
-    selected_game = self.rules.games[action]
-
-    return selected_game, prob
+    return allowed_games
 
   def retrieve_reward(self, reward):
     steps =  len(self.memory.states) - len(self.memory.rewards)

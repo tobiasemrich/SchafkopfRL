b11f1e23a9afb627a2564d103621c44984e5181e
Tobias Emrich <tobias.emrich@harman.com>
2020-12-18 14:35:56 +0100

    small changes to immitation policy

--- schafkopfrl/models/immitation_policy.py
+++ schafkopfrl/models/immitation_policy.py
@@ -6,6 +6,7 @@ from torch.autograd import Variable
 from torch.distributions import Categorical
 
 from rules import Rules
+from settings import Settings
 from utils import two_hot_encode_game, two_hot_encode_card, one_hot_cards, one_hot_games
 
 
@@ -58,4 +59,114 @@ class ImmitationPolicy(nn.Module):
 
         ax = ax.masked_fill(allowed_actions == 0, -1e9)
 
-        return ax
+        return ax, 0
+
+    def preprocess(state):
+        """
+        state_size:
+        - info_vector: 70 (74)
+          - game_stage: 11
+          - game_type: 7 [two bit encoding]
+          - game_player: 4
+          - contra_retour: 8
+          - first_player: 4
+          - current_scores: 4 (divided by 120 for normalization purpose)
+          - remaining cards: 32
+          (- teams: 4 [bits of players are set to 1])
+        - game_history: x * 16
+            - course_of_game: x * (12 + 4) each played card in order plus the player that played it
+        - current_trick: x * 16
+            - current_trick: x * (12 + 4) each played card in order plus the player that played it
+
+        action_size (43):
+         - games: 9
+         - contra/double: 2
+         - cards:  32
+        """
+
+        game_state = state["game_state"]
+        player_cards = state["current_player_cards"]
+        allowed_actions = state["allowed_actions"]
+
+        ############### gamestate ##################
+        ego_player = game_state.current_player
+
+        # game stage
+        game_stage = np.zeros(11)
+        if game_state.game_stage == Rules.BIDDING:
+            game_stage[0] = 1
+        elif game_state.game_stage == Rules.CONTRA:
+            game_stage[1] = 1
+        elif game_state.game_stage == Rules.RETOUR:
+            game_stage[2] = 1
+        else:
+            game_stage[3 + game_state.trick_number] = 1
+
+        game_enc = two_hot_encode_game(game_state.game_type)
+
+        game_player_enc = np.zeros(4)
+        if game_state.game_player != None:
+            game_player_enc[(game_state.game_player - ego_player) % 4] = 1
+
+        contra_retour = np.zeros(8)
+        for p in range(4):
+            if game_state.contra[p]:
+                contra_retour[(p - ego_player) % 4] = 1
+        for p in range(4):
+            if game_state.retour[p]:
+                contra_retour[4 + (p - ego_player) % 4] = 1
+
+        first_player_enc = np.zeros(4)
+        first_player_enc[(game_state.first_player - ego_player) % 4] = 1
+
+        # course of game
+        # course_of_game_enc = [torch.zeros(16).float().to(device='cuda')]
+        course_of_game_enc = np.zeros((1, 16))
+        current_trick_enc = np.zeros((1, 16))
+        for trick in range(len(game_state.course_of_game)):
+            for card in range(len(game_state.course_of_game[trick])):
+                if game_state.course_of_game[trick][card] == [None, None]:
+                    continue
+                else:
+                    card_player = game_state.first_player
+                    if trick != 0:
+                        card_player = game_state.trick_owner[trick - 1]
+                    card_player = (card_player + card) % 4
+                    card_player_enc = np.zeros(4)
+                    card_player_enc[(card_player - ego_player) % 4] = 1
+                    if trick != game_state.trick_number:
+                        course_of_game_enc = np.vstack((course_of_game_enc, np.append(
+                            np.array(two_hot_encode_card(game_state.course_of_game[trick][card])), card_player_enc)))
+                    else:
+                        current_trick_enc = np.vstack((current_trick_enc, np.append(
+                            np.array(two_hot_encode_card(game_state.course_of_game[trick][card])), card_player_enc)))
+
+        info_vector = np.concatenate((game_stage, game_enc, game_player_enc, contra_retour, first_player_enc,
+                                      np.true_divide(game_state.scores, 120),
+                                      one_hot_cards(player_cards)))  # , team_encoding
+
+        # return torch.tensor(info_vector).float().to(device='cuda')
+        # return [torch.tensor(info_vector).float().to(device='cuda'), course_of_game_enc]
+        if course_of_game_enc.shape[0] > 1:
+            course_of_game_enc = np.delete(course_of_game_enc, 0, 0)
+        course_of_game_enc = torch.tensor(course_of_game_enc).float().to(device=Settings.device)
+        course_of_game_enc = course_of_game_enc.view(len(course_of_game_enc), 1, 16)
+
+        if current_trick_enc.shape[0] > 1:
+            current_trick_enc = np.delete(current_trick_enc, 0, 0)
+        current_trick_enc = torch.tensor(current_trick_enc).float().to(device=Settings.device)
+        current_trick_enc = current_trick_enc.view(len(current_trick_enc), 1, 16)
+
+        ############### allowed actions ##################
+        allowed_actions_enc = np.zeros(43)
+        if game_state.game_stage == Rules.BIDDING:
+            allowed_actions_enc[0:9] = one_hot_games(allowed_actions)
+        elif game_state.game_stage == Rules.CONTRA or game_state.game_stage == Rules.RETOUR:
+            allowed_actions_enc[10] = 1
+            if np.any(allowed_actions):
+                allowed_actions_enc[9] = 1
+        else:
+            allowed_actions_enc[11:] = one_hot_cards(allowed_actions)
+
+        return [torch.tensor(info_vector).float().to(device=Settings.device), course_of_game_enc, current_trick_enc,
+                torch.tensor(allowed_actions_enc).float().to(device=Settings.device)]
--- schafkopfrl/trainer/immitation_trainer.py
+++ schafkopfrl/trainer/immitation_trainer.py
@@ -22,6 +22,8 @@ rules = Rules()
 
 def main():
 
+  # loading initial policy
+  immitation_policy = ImmitationPolicy().to(Settings.device)
 
   states = []
   actions = []
@@ -32,7 +34,7 @@ def main():
     game = games[g]
     if len(game.sonderregeln) == 0:
       count += 1
-      game_states, game_actions = get_states_actions(game)
+      game_states, game_actions = get_states_actions(game, immitation_policy)
       states += game_states
       actions += game_actions
 
@@ -66,8 +68,7 @@ def main():
   tb.configure(argv=[None, '--logdir', Settings.runs_folder])
   tb.launch()
 
-  # loading initial policy
-  immitation_policy = ImmitationPolicy().to(Settings.device)
+
   # take the newest generation available
   count = max_gen = 0
   generations = [int(f[:8]) for f in listdir(Settings.checkpoint_folder) if f.endswith(".pt")]
@@ -130,13 +131,7 @@ def main():
 
 
 
-
-
-
-
-
-
-def get_states_actions(game_transcript):
+def get_states_actions(game_transcript, policy):
 
   states = []
   actions = []
@@ -144,7 +139,7 @@ def get_states_actions(game_transcript):
   schafkopf_env = SchafkopfEnv()
 
   state, _, _ = schafkopf_env.set_state(PublicGameState(3), [game_transcript.player_hands[i] for i in range(4)])
-  states.append(preprocess(state))
+  states.append(policy.preprocess(state))
 
   #Bidding stage
 
@@ -189,7 +184,7 @@ def get_states_actions(game_transcript):
     actions.append(preprocess_action(Rules.BIDDING, action))
     state, _, _ = schafkopf_env.step(action)
     if not (len(game_transcript.bidding_round) == 4 and i == 3): #don't take the last state of the game into the dataset
-      states.append(preprocess(state))
+      states.append(policy.preprocess(state))
 
   if len(game_transcript.bidding_round) != 4: # if not all said weiter
 
@@ -202,7 +197,7 @@ def get_states_actions(game_transcript):
         action = True
       actions.append(preprocess_action(Rules.CONTRA, action))
       state, _, _ = schafkopf_env.step(action)
-      states.append(preprocess(state))
+      states.append(policy.preprocess(state))
 
     # RETOUR stage
     if len(con_ret) > 0:
@@ -212,7 +207,7 @@ def get_states_actions(game_transcript):
           action = True
         actions.append(preprocess_action(Rules.RETOUR, action))
         state, _, _ = schafkopf_env.step(action)
-        states.append(preprocess(state))
+        states.append(policy.preprocess(state))
 
     # TRICK stage
 
@@ -222,7 +217,7 @@ def get_states_actions(game_transcript):
         actions.append(preprocess_action(Rules.TRICK, action))
         state, _, _ = schafkopf_env.step(action)
         if not (trick == 7 and c == 3): # all but the last state
-          states.append(preprocess(state))
+          states.append(policy.preprocess(state))
 
   return states, actions
 
@@ -241,125 +236,6 @@ def preprocess_action(stage, action):
   action_representation[index] = 1
   #return torch.tensor(action_representation).float()
   return torch.tensor(index, dtype=torch.long)
-
-def preprocess(state):
-  """
-  state_size:
-  - info_vector: 70 (74)
-    - game_stage: 11
-    - game_type: 7 [two bit encoding]
-    - game_player: 4
-    - contra_retour: 8
-    - first_player: 4
-    - current_scores: 4 (divided by 120 for normalization purpose)
-    - remaining cards: 32
-    (- teams: 4 [bits of players are set to 1])
-  - game_history: x * 16
-      - course_of_game: x * (12 + 4) each played card in order plus the player that played it
-  - current_trick: x * 16
-      - current_trick: x * (12 + 4) each played card in order plus the player that played it
-
-  action_size (43):
-   - games: 9
-   - contra/double: 2
-   - cards:  32
-  """
-
-  game_state = state["game_state"]
-  player_cards = state["current_player_cards"]
-  allowed_actions = state["allowed_actions"]
-
-  ############### gamestate ##################
-  ego_player = game_state.current_player
-
-  # game stage
-  game_stage = np.zeros(11)
-  if game_state.game_stage == Rules.BIDDING:
-    game_stage[0] = 1
-  elif game_state.game_stage == Rules.CONTRA:
-    game_stage[1] = 1
-  elif game_state.game_stage == Rules.RETOUR:
-    game_stage[2] = 1
-  else:
-    game_stage[3 + game_state.trick_number] = 1
-
-  game_enc = two_hot_encode_game(game_state.game_type)
-
-  game_player_enc = np.zeros(4)
-  if game_state.game_player != None:
-    game_player_enc[(game_state.game_player - ego_player) % 4] = 1
-
-  contra_retour = np.zeros(8)
-  for p in range(4):
-    if game_state.contra[p]:
-      contra_retour[(p - ego_player) % 4] = 1
-  for p in range(4):
-    if game_state.retour[p]:
-      contra_retour[4 + (p - ego_player) % 4] = 1
-
-  first_player_enc = np.zeros(4)
-  first_player_enc[(game_state.first_player - ego_player) % 4] = 1
-  '''
-  team_encoding = np.zeros(4)
-  if game_state.get_player_team() != [None]:
-      player_team = [(t-ego_player)%4 for t in game_state.get_player_team()]
-
-      if game_state.game_type[1] != 0 and len(player_team) == 1:
-          team_encoding[player_team] = 1
-      elif game_state.game_type[1] == 0 and len(player_team) == 2:
-          team_encoding[player_team] = 1
-  '''
-
-  # course of game
-  # course_of_game_enc = [torch.zeros(16).float().to(device='cuda')]
-  course_of_game_enc = np.zeros((1, 16))
-  current_trick_enc = np.zeros((1, 16))
-  for trick in range(len(game_state.course_of_game)):
-    for card in range(len(game_state.course_of_game[trick])):
-      if game_state.course_of_game[trick][card] == [None, None]:
-        continue
-      else:
-        card_player = game_state.first_player
-        if trick != 0:
-          card_player = game_state.trick_owner[trick - 1]
-        card_player = (card_player + card) % 4
-        card_player_enc = np.zeros(4)
-        card_player_enc[(card_player - ego_player) % 4] = 1
-        if trick != game_state.trick_number:
-          course_of_game_enc = np.vstack((course_of_game_enc, np.append(
-            np.array(two_hot_encode_card(game_state.course_of_game[trick][card])), card_player_enc)))
-        else:
-          current_trick_enc = np.vstack((current_trick_enc, np.append(
-            np.array(two_hot_encode_card(game_state.course_of_game[trick][card])), card_player_enc)))
-
-  info_vector = np.concatenate((game_stage, game_enc, game_player_enc, contra_retour, first_player_enc,
-                                np.true_divide(game_state.scores, 120), one_hot_cards(player_cards)))  # , team_encoding
-
-  # return torch.tensor(info_vector).float().to(device='cuda')
-  # return [torch.tensor(info_vector).float().to(device='cuda'), course_of_game_enc]
-  if course_of_game_enc.shape[0] > 1:
-    course_of_game_enc = np.delete(course_of_game_enc, 0, 0)
-  course_of_game_enc = torch.tensor(course_of_game_enc).float().to(device=Settings.device)
-  course_of_game_enc = course_of_game_enc.view(len(course_of_game_enc), 1, 16)
-
-  if current_trick_enc.shape[0] > 1:
-    current_trick_enc = np.delete(current_trick_enc, 0, 0)
-  current_trick_enc = torch.tensor(current_trick_enc).float().to(device=Settings.device)
-  current_trick_enc = current_trick_enc.view(len(current_trick_enc), 1, 16)
-
-  ############### allowed actions ##################
-  allowed_actions_enc = np.zeros(43)
-  if game_state.game_stage == Rules.BIDDING:
-    allowed_actions_enc[0:9] = one_hot_games(allowed_actions)
-  elif game_state.game_stage == Rules.CONTRA or game_state.game_stage == Rules.RETOUR:
-    allowed_actions_enc[10] = 1
-    if np.any(allowed_actions):
-      allowed_actions_enc[9] = 1
-  else:
-    allowed_actions_enc[11:] = one_hot_cards(allowed_actions)
-
-  return [torch.tensor(info_vector).float().to(device=Settings.device), course_of_game_enc, current_trick_enc,
-          torch.tensor(allowed_actions_enc).float().to(device=Settings.device)]
 
 
 if __name__ == '__main__':
